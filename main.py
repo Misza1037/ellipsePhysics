b7adc41678f1f306fb9ecce253d45c62d0a71ca6
sujemuje <84621465+sujemuje@users.noreply.github.com>
2021-05-29 16:41:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 #main.py
+from math import sqrt
 import pygame
 from time import sleep
 pygame.init()
-screenSize = [800,600]
+
+screenSize = (800,600)
 screen = pygame.display.set_mode(screenSize)
 screen.fill((255, 255, 255))
-sx = 100
-sy = 100
-rx = 600
-ry = 400
-pygame.draw.ellipse(screen,(0,0,0),(sx, sy, rx, ry))
 
+dx = screenSize[0]//2
+dy = screenSize[1]//2
+rx = 300
+ry = 200
+
+
+def pochodna(x):
+    return sqrt(ry**2 * (1 - (x**2 / rx**2)))
+
+
+pygame.draw.ellipse(screen,(0,0,0),(dx-rx, dy-ry, 2*rx, 2*ry))
 pygame.display.flip()
+
 sleep(4)
 pygame.quit()
